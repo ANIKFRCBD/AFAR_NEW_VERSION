5384980c7de4d737c0bc191c2cfc5b9ca233b33d
অনিক মল্লিক <anikmallick.frcbd@gmail.com>
2024-02-11 21:01:26 +0600

    anik fixed depreciation view

--- afar_project/depreciation/views.py
+++ afar_project/depreciation/views.py
@@ -4,83 +4,72 @@ from datetime import datetime
 from afar_project.views import calculate_values,calculate_costs
 
 
-
-# Create your views here.
+file_path='csv_path/sample/asset_register.xlsx'
 def dep(request):
-    # Read the existing data from the Excel file
-    file_path = 'csv_path/sample/asset_registerxxx.xlsx'
+    #call functions
+    dep_sheet,colums_to_add,year_data=dep_sheet_maker(request)
+    print(type(dep_sheet))
+    #peocess the html
+    dep_data=dep_sheet #for Depreciation calculation
+    html=dep_sheet.to_html(index=False) # for html template
+    print(type(html))
+    header_html = html.split('<tbody>')[0]  # Extract headers part
+    rows_html = '<tbody>' + html.split('<tbody>')[1]  # Extract rows part
+
+
+    return render(request, 'frc_dep.html', {'header_html': header_html, 'rows_html': rows_html})
+# Create your views here.
+def dep_sheet_maker(request):
     df = pd.read_excel(file_path)
-    print(df.head())
-    df.fillna('', inplace=True)
-    numeric_cols = df.select_dtypes(include='number').columns  # Select numeric columns
-    df[numeric_cols] = df[numeric_cols].applymap(lambda x: f'{x:.2f}' if not pd.isnull(x) else '')  # Format 
-
-    # Extract unique values from the first column (assuming it's the financial year column)
-    unique_values = df.iloc[:, 0].unique().tolist()
-
-    # Determine the smallest financial year from the existing data
-    years = [int(year[:4]) for year in unique_values if year[:4].isdigit()]  # Extract year part and convert to integer
-    smallest_year = min(years) if years else None
-
-    # Get the current financial year based on the current date
-    if smallest_year is not None:
-        current_date = datetime.now()
-        current_year = current_date.year
-        current_financial_year = f"{current_year - 1}-{current_year}"
-
-        # Calculate the range of financial years to be added
-        if current_date.month > 6:
-            financial_years_to_add = [f"{year}-{year+1}" for year in range(smallest_year, current_year + 1)]
-        else:
-            financial_years_to_add = [f"{year-1}-{year}" for year in range(smallest_year, current_year)]
-
-        # Determine additional columns needed and set their values to 0
-        for year in financial_years_to_add:
-            df[year] = 0
-        
-        extra_columns = df.columns[20:]  # Assuming extra columns start from column index 23
-        for index, row in df.iloc[0:].iterrows():
-            calculated_values = calculate_values(row)
-            df.loc[index, extra_columns] = calculated_values
-        
-        # for column_index in range(22, len(df.columns)):
-        #     column_name = df.columns[column_index]
-        #     # Iterate over rows within the column (excluding the header row)
-        #     for index, value in df.iloc[1:, column_index].items():
-        #         if(df.iloc[index, 0] != column_name):
-        #             df.at[index,column_name] = 0
-        #         else:
-        #             continue
-            
-        for index, row in df.iloc[1:].iterrows():
-            for column_name in df.columns[20:]:
-                # Inner loop for columns
-                if(df.iloc[index, 0] != column_name):
-                    df.at[index,column_name] = 0
-                else:
-                    break
-
-        for index, row in df.iloc[0:].iterrows():
-            cn = 0
-            e_life = float(df.iloc[index, 16]) if df.iloc[index, 16] != '' else 1
-            for column_name in df.columns[20:]:
-                if(e_life <= cn):
-                    df.at[index, column_name] = 0
-                else:
-                    cn = cn + 1
-
-    # Convert DataFrame to HTML
-    numeric_cols = df.select_dtypes(include='number').columns  # Select numeric columns
-    df[numeric_cols] = df[numeric_cols].applymap(lambda x: f'{x:.2f}' if not pd.isnull(x) else '')  # Format 
+    pd.set_option('display.float_format', '{:.2f}'.format)
+    df=df[['Financial Year','Asset Code','Purchase date','Sl ','Bill no','Economic Code','Category','Name of Item','Brand Name','Model/Type','Units','Modified Number','Price','Sold (unit)','Cost of Assets Sold','Current Balance']]
+    df['Accumulated Depreciation']=0
+    df['Units']=df['Units'].fillna(0)
+    df['Sold (unit)']=df['Sold (unit)'].fillna(0)
+    df['Modified Number']=df['Modified Number'].fillna(0)
+    df[['Cost of Assets Sold']]=df[['Cost of Assets Sold']].fillna(0)
+    df['Current Balance']=df['Price']-df['Cost of Assets Sold']
+    years=df['Financial Year'].drop_duplicates()
+    current_year=datetime.now()
+    # print(current_year.year)
+    year={}
+    #getting year Values as a list to iterate
+    for i in years:
+        d=i.split('-')
+        year[f'{i}']=int(d[1])
+    val=year.values()
+    years=[]
+    for i in val:
+        years.append(i)
+    years.sort()
+    # find out the numbers of years to add
+    columns_to_create=current_year.year-int(years[0])
+    #check whether any new year-based column is to be added
+    if len(years)<columns_to_create:
+    # print(columns_to_create)
+        years_new=[]
+        for i in range(columns_to_create+1):
+            p=int(years[0])+i
+            years_new.append(p)
+        # years_new.append(list[i])
+        years_dict={}
+        for i in years_new:
+            years_dict[f'{int(i)-1}-{i}']=int(i)
+        for i in years_dict.keys():
+            df[i]=0
+        columns_to_add=int(columns_to_create)
+        year_data=years_dict
+    else:
+        #if no more year based columns to be added
+        for i in years:
+            df[i]=0
+        columns_to_add=len(years)
+        year_data=years
+    #Write it to excel file
+    df.to_excel("depreciation.xlsx",index=False)
     
-    excel_html = df.to_html(index=False)
+    return df,columns_to_add,year_data
+# def depreciation_calculation(request):
 
-    # Split the HTML table into headers and rows
-    header_html = excel_html.split('<tbody>')[0]  # Extract headers part
-    rows_html = '<tbody>' + excel_html.split('<tbody>')[1]  # Extract rows part
-    print(df.head())
 
-    # You can also save the modified DataFrame back to an Excel file
-    df.to_excel('csv_path/sample/modified_asset_register.xlsx', index=False)
-
-    return render(request, 'frc_dep.html', {'header_html': header_html, 'rows_html': rows_html, 'unique_values': unique_values})
+    
