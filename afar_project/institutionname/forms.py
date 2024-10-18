01d9d7579637250577ca74aded3f138b98a084d6
অনিক মল্লিক <anikmallick.frcbd@gmail.com>
2024-10-18 16:14:57 +0600

    main changed done

--- afar_project/institutionname/forms.py
+++ afar_project/institutionname/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.core.validators import FileExtensionValidator
 
 class institutionname_form(forms.Form):
     Name = forms.CharField(max_length=255)
-    Website = forms.URLField()
+    Website = forms.CharField(max_length=255)
     District = forms.CharField(max_length=255)
     address = forms.CharField(max_length=255)
     authority = forms.CharField(max_length=255)
-    document = forms.FileField()
+    document = forms.FileField(validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
--- afar_project/institutionname/views.py
+++ afar_project/institutionname/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from .forms import institutionname_form
+import json as j
+import os
+from django.conf import settings
 
 def institutionname(request):
-    context = {"form": None}
+    context = {"form": institutionname_form()}
     if request.method == "POST":
         data = institutionname_form(request.POST, request.FILES)
+        # Checkvalidation
         if data.is_valid():
             institution_name = data.cleaned_data['Name']
             website = data.cleaned_data['Website']
@@ -16,14 +20,18 @@ def institutionname(request):
             logo_file = data.cleaned_data['document']
             
             fs = FileSystemStorage(location='institutionmedia')
-            file_name = fs.save(logo_file.name, logo_file)
+            file_name = fs.save(f"{institution_name}.png", logo_file)
             
             list_of_elements = [institution_name, website, district, address, ministry, file_name]
-            context.update({'form_data': list_of_elements})
+            #save file in JSON
+            information={"Institute_name":institution_name,"website":website,"district":district,"address":address,"ministry":ministry,"file_location":os.path.join(settings.BASE_DIR,"institutionmedia",f"{institution_name}.png")}
+            file_path = os.path.join(settings.BASE_DIR, 'institutionname', f"{institution_name}.json")
+            with open(file_path,"w") as file:
+                j.dump(information,file,indent=4)
+            context.update({'form_data': list_of_elements, 'info': list_of_elements})
         else:
             context.update({'form': data})
     else:
-        data = 0
-        context.update({'form': data})
+        context.update({'info': None})
     
     return render(request, "institution_registration.html", context)
