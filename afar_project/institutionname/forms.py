6894e233659e3b592ce529595d3701cc1f04040f
অনিক মল্লিক <anikmallick.frcbd@gmail.com>
2024-10-18 16:26:19 +0600

    main changed done

--- afar_project/institutionname/forms.py
+++ afar_project/institutionname/forms.py
@@ -5,6 +5,6 @@ class institutionname_form(forms.Form):
     Name = forms.CharField(max_length=255)
     Website = forms.CharField(max_length=255)
     District = forms.CharField(max_length=255)
-    address = forms.CharField(max_length=255)
+    street_address = forms.CharField(max_length=255)
     authority = forms.CharField(max_length=255)
-    document = forms.FileField(validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+    logo = forms.FileField(validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
--- afar_project/institutionname/views.py
+++ afar_project/institutionname/views.py
@@ -15,20 +15,20 @@ def institutionname(request):
             institution_name = data.cleaned_data['Name']
             website = data.cleaned_data['Website']
             district = data.cleaned_data['District']
-            address = data.cleaned_data['address']
+            street_address = data.cleaned_data['street_address']
             ministry = data.cleaned_data['authority']
-            logo_file = data.cleaned_data['document']
+            logo_file = data.cleaned_data['logo']
             
             fs = FileSystemStorage(location='institutionmedia')
             file_name = fs.save(f"{institution_name}.png", logo_file)
             
-            list_of_elements = [institution_name, website, district, address, ministry, file_name]
+            list_of_elements = [institution_name, website, district, street_address, ministry, file_name]
             #save file in JSON
-            information={"Institute_name":institution_name,"website":website,"district":district,"address":address,"ministry":ministry,"file_location":os.path.join(settings.BASE_DIR,"institutionmedia",f"{institution_name}.png")}
+            information={"Institute_name":institution_name,"website":website,"district":district,"address":street_address,"ministry":ministry,"file_location":os.path.join(settings.BASE_DIR,"institutionmedia",f"{institution_name}.png")}
             file_path = os.path.join(settings.BASE_DIR, 'institutionname', f"{institution_name}.json")
             with open(file_path,"w") as file:
                 j.dump(information,file,indent=4)
-            context.update({'form_data': list_of_elements, 'info': list_of_elements})
+            context.update({'form_data': list_of_elements, 'info': list_of_elements,"file+location":information["file_location"]})
         else:
             context.update({'form': data})
     else:
